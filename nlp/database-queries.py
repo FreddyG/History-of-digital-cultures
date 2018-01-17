46d26b460414ccfb39356a36a7e49e527f0c5d63
KjellZijlemaker <kjell.zijlemaker@live.nl>
2018-01-17 15:40:20 +0100

    Added some queries for obtaining total message count and first/last entry for each/all cafes

--- nlp/database-queries.py
+++ nlp/database-queries.py
@@ -7,10 +7,40 @@ def __main__():
     cnx = mysql.connector.connect(user='root', database='testing')
     cursor = cnx.cursor(buffered=True)
 
+    tables = []
+    finaltables = []
+    timestamps = []
+
     # Show tables
     table_cursor = show_tables(cursor)
     for i in table_cursor:
-        print(i[0])
+        tables.append(i[0])
+
+    total_messages = 0
+    # Select the tables on which the messages are greater than 10
+    for t in tables:
+        amount_cursor = get_total_amount_messages("`" + t + "`", cursor)
+        for k in amount_cursor:
+            total_messages += k[0]
+            if k[0] >= 10:
+                finaltables.append(t)
+
+    print("Total messages: " + str(total_messages))
+
+    # Query the tables and get first / last message of each café
+    for new_tables in finaltables:
+        cafe_first_last_messages_cursor = query_cafe_first_last_messages("`" + new_tables + "`", "desc", cursor)
+        for messages in cafe_first_last_messages_cursor:
+            print(
+                messages[0] + ", " + messages[1] + ", timestamp: " + str(datetime.datetime.fromtimestamp(messages[2])))
+            timestamps.append(messages[2])
+
+    # Get oldest timestamp and newest timestamp + print it
+    youngest_timestamp = min(timestamps)
+    now = datetime.datetime.now().timestamp()
+    oldest_timestamp = max(dt for dt in timestamps if dt < now)
+    print(str(datetime.datetime.fromtimestamp(youngest_timestamp)) + ", " + str(datetime.datetime.fromtimestamp
+                                                                                (oldest_timestamp)))
 
     # Get all messages from the designated cafe
     query_cafe_cursor = query_cafe("europa", cursor)
@@ -60,7 +90,14 @@ def query_cafe_with_timestamp(cafe, timestampbegin, timestampend, cursor):
 
 # Returns first or last message from cafe
 def query_cafe_first_last_messages(cafe, mode, cursor):
-    query = ("SELECT message, timestamp FROM " + cafe + " ORDER BY timestamp " + mode + " limit 1")
+    query = ("SELECT cafe, message, timestamp FROM " + cafe + " ORDER BY timestamp " + mode + " limit 1")
+    cursor.execute(query)
+
+    return cursor
+
+
+def get_total_amount_messages(cafe, cursor):
+    query = ("SELECT count(message) FROM " + cafe)
     cursor.execute(query)
 
     return cursor
