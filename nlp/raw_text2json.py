cfe0daf1d8507d9a68f6935149fe0dd0d2337193
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2018-01-20 19:45:05 +0100

    Train LSI on all texts
    Then classify monthly aggregated massages using trained model

--- nlp/raw_text2json.py
+++ nlp/raw_text2json.py
@@ -14,8 +14,8 @@ RESOURCES = ['corpora/stopwords',
              'tokenizers/punkt']
 
 
-def read_files():
-    asd = {}
+def read_files2():
+    res = []
     directory = 'data'
     onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
 
@@ -24,15 +24,12 @@ def read_files():
         name = file.split('.')[0]
         cafename = name.split('-')[0]
         date = name.split('-')[1] + '-' + name.split('-')[2]
-        content = f.read().replace('\n', ' ')[:10]
+        content = f.read().replace('\n', ' ')
 
-        if date in asd:
-            asd[date].append(content)
-        else:
-            asd[date] = []
-
+        res.append((cafename, date, content))
         f.close()
-    return asd
+
+    return res
 
 
 def check_nltk_resources(path):
@@ -42,12 +39,11 @@ def check_nltk_resources(path):
         except LookupError:
             nltk.download(res.split('/')[1], path)
 
+
 def get_stopwords():
     lines = [line.rstrip('\n') for line in open('our_stopwords.txt')]
     return nltk.corpus.stopwords.words('dutch') + lines
 
-check_nltk_resources(NLTK_DATA_PATH)
-stopwords = get_stopwords()
 
 def tokenize(text):
     # first tokenize by sentence, then by word to ensure that punctuation
@@ -64,42 +60,49 @@ def tokenize(text):
     return filtered_tokens
 
 
-def dump_keywords(lst):
-    topic_keywords = []
-    for item in lst:
-        date = item[0]
-        topics = item[1]
-        dictionary = {"date": date, "topics": []}
-        for topic in topics:
-            topic_keywords1 = []
-            for keyword in topic[1]:
-                d={"keyword":keyword[0], "weight":keyword[1]}
-                topic_keywords1.append(d)
-            dictionary["topics"].append(topic_keywords1)
-        topic_keywords.append(dictionary)
-
-    import json
-    with open('keywords.json', 'w') as f:
-        json.dump(topic_keywords, f, ensure_ascii=False)
+check_nltk_resources(NLTK_DATA_PATH)
+stopwords = get_stopwords()
 
 
 if __name__ == "__main__":
-    files = read_files()
-    lst = []
-    for time_period, texts in files.items():
-        tokenized = [tokenize(text) for text in texts]
-        dictionary = corpora.Dictionary(tokenized)
-        corpus = [dictionary.doc2bow(text) for text in tokenized]
+    in_memory_files = read_files2()
 
-        tfidf = models.TfidfModel(corpus)   # train model
-        corpus_tfidf = tfidf[corpus]        # transform documents
+    tokenized = [tokenize(x[2]) for x in in_memory_files]
+    dictionary = corpora.Dictionary(tokenized)
+    corpus = [dictionary.doc2bow(text) for text in tokenized]
 
-        # initialize an LSI transformation
-        lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
-        # create a double wrapper over the original corpus: bow->tfidf->fold-in-lsi
-        corpus_lsi = lsi[corpus_tfidf] 
-        topics = lsi.show_topics(formatted=False)
+    tfidf = models.TfidfModel(corpus)   # train model
+    corpus_tfidf = tfidf[corpus]        # transform documents
 
-        lst.append((time_period, topics))
+    # initialize an LSI transformation
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=300)
 
-    dump_keywords(lst)
+    # text = "Hallo meiden van TEchnika 10 Zijn er meisjes van de vierwindstreken in het cafe Ik Pia zit nu ook in het cafe en wil graag aan jullie vragen hoe het vandaag gaat met de Meisjes Internet Club."
+    # bow_vector = dictionary.doc2bow(tokenize(text))
+    # print([(dictionary[id], count) for id, count in bow_vector])
+
+    # lsi_vector = lsi[bow_vector]
+    # print(lsi_vector)
+    # print(lsi.print_topic(max(lsi_vector, key=lambda item: item[1])[0]))
+
+    dic = {}
+    for f in in_memory_files:
+        if not f[0] in dic:
+            dic[f[0]] = []
+
+    for f in in_memory_files:
+        bow_vector = dictionary.doc2bow(tokenize(f[2]))
+        lsi_vector = lsi[bow_vector]
+        if not lsi_vector:
+            continue
+        keywords = lsi.show_topic(max(lsi_vector, key=lambda item: item[1])[0])
+
+        lst = []
+        for word_weight in keywords:
+            lst.append({"keyword":word_weight[0], "weight":word_weight[1]})
+
+        dic[f[0]].append({f[1]: lst})
+
+    import json
+    with open('keywords1.json', 'w') as f:
+        json.dump(dic, f, ensure_ascii=False)
